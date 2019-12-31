24e45d342edec60d1c3925672bb499650c6ebb10
Cristhian Ferrufino <c.ferrufino@zertificon.com>
2019-12-31 11:05:03 +0100

    🚀 Init commit

---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    printWidth: 120,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'none',
+    bracketSpacing: false,
+    jsxBracketSameLine: true,
+    arrowParens: 'always',
+    rangeStart: 0,
+    rangeEnd: Infinity,
+    requirePragma: false,
+    insertPragma: false,
+    proseWrap: 'preserve'
+};
